3e8417c684e6bf6bd115443e82b90962e47c65b6
YerayN <ny93yeray@gmail.com>
2025-10-07 12:49:00 +0200

    Iniciando despliegue

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -e
 
 cd backend
-pip3 install -r ../requirements.txt
-python3 manage.py migrate
-python3 manage.py collectstatic --noinput
-python3 manage.py runserver 0.0.0.0:$PORT
+pip install -r ../requirements.txt
+python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py runserver 0.0.0.0:$PORT
